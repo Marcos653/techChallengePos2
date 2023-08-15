bd0698866aabf8b5f3f0b8d494955bdebdd2049a
Marcos653 <marcos.costas@xbrain.com.br>
2023-08-14 21:10:12 -0300

    feat: set up

---
+++ eletrodomesticos_gestao_api/src/main/java/com/techchallenge/eletrodomesticos/EletrodomesticosApplication.java
@@ -0,0 +1,13 @@
+package com.techchallenge.eletrodomesticos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletrodomesticosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletrodomesticosApplication.class, args);
+	}
+
+}
---
+++ eletrodomesticos_gestao_api/src/test/java/com/techchallenge/eletrodomesticos/EletrodomesticosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techchallenge.eletrodomesticos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletrodomesticosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
